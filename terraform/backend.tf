b25db2dbbd8978a6ce5408207368a92eda964424
vignesh-a-nt07 <vignesh.a@navattech.com>
2026-02-19 09:49:35 +0530

    updated statefile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ecom-vicky-terraform-state-492000"
-    key            = "lms/dev/terraform.tfstate"
+    key            = "ecom/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-ecom-lock"
     encrypt        = true
